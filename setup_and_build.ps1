4b2561d77bc6049fd8b4886ee3656e1dfbd61760
Yoel Cohen <yoel@example.com>
2026-08-09 05:08:27 +0200

    Fix identite ad-hoc CODE_SIGN_IDENTITY='-'

--- setup_and_build.ps1
+++ setup_and_build.ps1
@@ -34,13 +34,13 @@ Write-Host "  [OK] Version: $version" -ForegroundColor Green
 Write-Host ""
 
 # -- ETAPE 2 : Commit et Push des fixes --
-Write-Host "[2/4] Commit et push du mode Archive pour compilation non signee..." -ForegroundColor Yellow
+Write-Host "[2/4] Commit et push de l'identite Ad-Hoc Sign (-)..." -ForegroundColor Yellow
 
 git config user.email "yoel@example.com"
 git config user.name "Yoel Cohen"
 
 git add .
-git commit -m "Passage en mode xcodebuild archive avec ad-hoc signing" --allow-empty
+git commit -m "Fix identite ad-hoc CODE_SIGN_IDENTITY='-'" --allow-empty
 git push -u origin main
 Write-Host "  [OK] Push reussi sur GitHub !" -ForegroundColor Green
 Write-Host ""
